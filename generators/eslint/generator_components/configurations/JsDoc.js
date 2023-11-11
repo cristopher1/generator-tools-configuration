4be88e86a28661485b66adf9367f6e08d7e0ef69
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-11 00:48:25 -0300

    feat(eslint): se agrega configuración usar eslint con jsdoc

---
+++ generators/eslint/generator_components/configurations/JsDoc.js
@@ -0,0 +1,20 @@
+export class JsDoc {
+  #addOverrides(eslintConfig) {
+    eslintConfig.overrides.push({
+      files: ['src/**/*.js'],
+      extends: ['plugin:jsdoc/recommended'],
+      plugins: ['jsdoc'],
+      rules: {
+        'jsdoc/tag-lines': 0,
+      },
+    })
+  }
+
+  addConfiguration(eslintConfig) {
+    this.#addOverrides(eslintConfig)
+  }
+
+  addDependencies(dependencies) {
+    dependencies.devDependencies['eslint-plugin-jsdoc'] = '^46.8.2'
+  }
+}
